b770b1676b6b100b57f9823cfec7eb82d13e22e4
Gema Aji Wardian <gema_wardian@hotmail.com>
2017-04-15 12:40:07 +0700

    Updated JQuery, and other JS stuff.

--- application/views/footer.php
+++ application/views/footer.php
@@ -9,11 +9,14 @@
 </footer>
 </div>
 
+  <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+  <script type="text/javascript" src="<?php echo base_url("assets/js/select2.full.js"); ?>"></script>
+  <script type="text/javascript" src="<?php echo base_url("assets/js/jquery.datatables.js"); ?>"></script>
   <script type="text/javascript" src="<?php echo base_url("assets/js/vendor.js"); ?>"></script>
+  <script type="text/javascript" src="<?php echo base_url("assets/js/chart.js"); ?>"></script>
+  <script type="text/javascript" src="<?php echo base_url("assets/js/jquery.autocomplete.js"); ?>"></script>
+  <script type="text/javascript" src="<?php echo base_url("assets/js/perfect-scrollbar.jquery.js"); ?>"></script>
   <script type="text/javascript" src="<?php echo base_url("assets/js/app.js"); ?>"></script>
-  <script type="text/javascript" src="<?php echo base_url("assets/js/moment.js"); ?>"></script>
-  <script type="text/javascript" src="<?php echo base_url("assets/js/transition.js"); ?>"></script>
-  <script type="text/javascript" src="<?php echo base_url("assets/js/collapse.js"); ?>"></script>
   <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap3-typeahead.js"); ?>"></script>
   <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap-datetimepicker.min.js"); ?>"></script>
 </body>
--- application/views/table_footer.php
+++ application/views/table_footer.php
@@ -1,6 +1,10 @@
 
-
+  <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+  <script type="text/javascript" src="<?php echo base_url("assets/js/select2.full.js"); ?>"></script>
+  <script type="text/javascript" src="<?php echo base_url("assets/js/jquery.datatables.js"); ?>"></script>
   <script type="text/javascript" src="<?php echo base_url("assets/js/vendor.js"); ?>"></script>
+  <script type="text/javascript" src="<?php echo base_url("assets/js/jquery.autocomplete.js"); ?>"></script>
+  <script type="text/javascript" src="<?php echo base_url("assets/js/perfect-scrollbar.jquery.js"); ?>"></script>
   <script type="text/javascript" src="<?php echo base_url("assets/js/app.js"); ?>"></script>
 </body>
 </html>
