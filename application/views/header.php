bd932824e001a3b3739210e8dcb1294a1a2d84b3
Gema Aji Wardian <gema_wardian@hotmail.com>
2017-04-15 11:45:04 +0700

    Updated CSS, Fonts and login improvement.

--- application/views/header.php
+++ application/views/header.php
@@ -5,15 +5,16 @@
   
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/vendor.css"); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/bootstrap.css"); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/font-awesome.css"); ?>">
+
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/datatables.bootstrap.css"); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/chartist.css"); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/highlight-js-theme.css"); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/select2.css"); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/perfect-scrollbar.css"); ?>">
   <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/flat-admin.css"); ?>">
 
-  <!-- Theme -->
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/theme/blue-sky.css"); ?>">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/theme/blue.css"); ?>">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/theme/red.css"); ?>">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/theme/yellow.css"); ?>">
-  
   <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/bootstrap-datetimepicker.css"); ?>">
 
   <script type="text/javascript" src="<?php echo base_url("assets/js/jquery.min.js"); ?>"></script>
--- application/views/login/login_view.php
+++ application/views/login/login_view.php
@@ -54,9 +54,5 @@
 </div>
 
   </div>
-  
-  <script type="text/javascript" src="../assets/js/vendor.js"></script>
-  <script type="text/javascript" src="../assets/js/app.js"></script>
-
 </body>
 </html>
