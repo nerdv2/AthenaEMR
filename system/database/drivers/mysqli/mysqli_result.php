75fa89b728e5bb43efbfea7198e1c2b0f8ee9d70
Gema Aji Wardian <gema_wardian@hotmail.com>
2017-04-15 09:47:12 +0700

    Updated to CI 3.1.4 https://www.codeigniter.com/user_guide/changelog.html#version-3-1-4

--- system/database/drivers/mysqli/mysqli_result.php
+++ system/database/drivers/mysqli/mysqli_result.php
@@ -183,12 +183,4 @@ class CI_DB_mysqli_result extends CI_DB_result {
 		return $this->result_id->fetch_object($class_name);
 	}
 
-	function next_result()
-	{
-		if (is_object($this->conn_id))
-		{
-			return mysqli_next_result($this->conn_id);
-		}
-	}
-
 }
