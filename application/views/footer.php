95b7a1e312d014bcc85b4f94d48068363dbb9488
Gema Aji Wardian <gema_wardian@hotmail.com>
2017-06-22 12:16:53 +0700

    Fixing placement of div error on footer.

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,12 +1,14 @@
+
   <footer class="app-footer"> 
-  <div class="row">
-    <div class="col-xs-12">
-      <div class="footer-copyright">
-        Copyright © 2016-2017 AthenaEMR Development Team / Gema Aji Wardian. For license info <a href="<?php echo base_url("LICENSE"); ?>">LICENSE</a>
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="footer-copyright">
+          Copyright © 2016-2017 AthenaEMR Development Team / Gema Aji Wardian. For license info <a href="<?php echo base_url("LICENSE"); ?>">LICENSE</a>
+        </div>
       </div>
     </div>
+  </footer>
   </div>
-</footer>
 </div>
 
   <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
--- application/views/table_footer.php
+++ application/views/table_footer.php
@@ -1,4 +1,16 @@
 
+  <footer class="app-footer"> 
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="footer-copyright">
+          Copyright © 2016-2017 AthenaEMR Development Team / Gema Aji Wardian. For license info <a href="<?php echo base_url("LICENSE"); ?>">LICENSE</a>
+        </div>
+      </div>
+    </div>
+  </footer>
+  </div>
+</div>
+
   <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
   <script type="text/javascript" src="<?php echo base_url("assets/js/select2.full.js"); ?>"></script>
   <script type="text/javascript" src="<?php echo base_url("assets/js/jquery.datatables.js"); ?>"></script>
