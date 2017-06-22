94d9516b839a824ba49b1b2f6490bd1ccc0ff1e6
Gema Aji Wardian <gema_wardian@hotmail.com>
2017-06-22 19:50:08 +0700

    Test CSRF protection to azure.

--- application/config/config.php
+++ application/config/config.php
@@ -28,7 +28,7 @@ date_default_timezone_set('Asia/Jakarta');
 $config['base_url'] = 'http://athenaemr.azurewebsites.net/';
 
 //local
-//$config['base_url'] = 'http://localhost/project_athena_emr/';
+//$config['base_url'] = 'http://localhost/AthenaEMR/';
 /*
 
 /*
@@ -454,7 +454,7 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_regenerate' = Regenerate token on every submission
 | 'csrf_exclude_uris' = Array of URIs which ignore CSRF checks
 */
-$config['csrf_protection'] = FALSE;
+$config['csrf_protection'] = TRUE;
 $config['csrf_token_name'] = 'csrf_test_name';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
 $config['csrf_expire'] = 7200;
