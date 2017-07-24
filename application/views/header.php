c860d4cc181bae550c4a7194ed94cfaaf3440a35
Gema Aji Wardian <gema_wardian@hotmail.com>
2017-07-24 13:13:53 +0700

    another escaping test.

--- application/views/header.php
+++ application/views/header.php
@@ -5,22 +5,22 @@
   
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="ee2acbd2d69a4d1f504abb12303112fe29ddc378" content="cde94c152ed833495af3b55314535aa09a584b9f" />
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/bootstrap.css"); ?>">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/font-awesome.css"); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo html_escape(base_url("assets/css/bootstrap.css")); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo html_escape(base_url("assets/css/font-awesome.css")); ?>">
 
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/datatables.bootstrap.css"); ?>">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/chartist.css"); ?>">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/highlight-js-theme.css"); ?>">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/select2.css"); ?>">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/perfect-scrollbar.css"); ?>">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/flat-admin.css"); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo html_escape(base_url("assets/css/datatables.bootstrap.css")); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo html_escape(base_url("assets/css/chartist.css")); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo html_escape(base_url("assets/css/highlight-js-theme.css")); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo html_escape(base_url("assets/css/select2.css")); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo html_escape(base_url("assets/css/perfect-scrollbar.css")); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo html_escape(base_url("assets/css/flat-admin.css")); ?>">
 
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/bootstrap-datetimepicker.css"); ?>">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/js/trumbowyg/ui/trumbowyg.min.css"); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo html_escape(base_url("assets/css/bootstrap-datetimepicker.css")); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo html_escape(base_url("assets/js/trumbowyg/ui/trumbowyg.min.css")); ?>">
 
-  <script type="text/javascript" src="<?php echo base_url("assets/js/jquery.min.js"); ?>"></script>
-  <script src="<?php echo base_url("assets/js/trumbowyg/trumbowyg.min.js"); ?>"></script>
-  <script src="<?php echo base_url("assets/js/trumbowyg/plugins/table/trumbowyg.table.min.js"); ?>"></script>
-  <script src="<?php echo base_url("assets/js/tinymce/tinymce.min.js"); ?>" type="text/javascript"></script>
+  <script type="text/javascript" src="<?php echo html_escape(base_url("assets/js/jquery.min.js")); ?>"></script>
+  <script src="<?php echo html_escape(base_url("assets/js/trumbowyg/trumbowyg.min.js")); ?>"></script>
+  <script src="<?php echo html_escape(base_url("assets/js/trumbowyg/plugins/table/trumbowyg.table.min.js")); ?>"></script>
+  <script src="<?php echo html_escape(base_url("assets/js/tinymce/tinymce.min.js")); ?>" type="text/javascript"></script>
   <script>tinymce.init({ selector:'textarea#textarea-tinymce', plugins:'table' });</script>
 </head>
